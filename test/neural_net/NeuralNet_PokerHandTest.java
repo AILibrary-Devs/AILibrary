7271ff04eb1cf67646495af86c65becc4ed3798a
Kevin Lawrence <kevinlawrence@outlook.com>
2017-10-30 07:54:09 -0400

    Poker Hands - First Implementation (doesn't work - Scaling?)

--- test/neural_net/NeuralNet_PokerHandTest.java
+++ test/neural_net/NeuralNet_PokerHandTest.java
@@ -6,10 +6,13 @@
 package neural_net;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -44,50 +47,114 @@ public class NeuralNet_PokerHandTest {
     public void tearDown() {
     }
 
+    private ArrayList<double[]> getDataFromFile(String filePath, int elements, int recordLimit) {
+        ArrayList<double[]> data = new ArrayList<>();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filePath).getFile());
+
+        try (Scanner scanner = new Scanner(file)) {
+            int counter = 0;
+            while (scanner.hasNextLine() && (counter < recordLimit)) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+
+                String[] list = line.split(",");
+
+                double[] input = new double[elements];
+                for (int i = 0; i < input.length; i++) {
+                    input[i] = Integer.parseInt(list[i]);
+                }
+
+                data.add(input);
+                counter++;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NeuralNet_PokerHandTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return data;
+    }
+
     /**
      * Test of train method, of class NeuralNet.
      * https://archive.ics.uci.edu/ml/machine-learning-databases/poker/
      *
-     *
      */
     @Test
-//    public void testPokerHand(String testLabel, int[] layerNodeCounts, double[][] inputValues, double[][] expectedValues, int iterationLimit, double accuracyLimit) {
-    public void testPokerHand(){//String testLabel, double[][] inputValues, double[][] expectedValues, int iterationLimit, double accuracyLimit) {
-
-        int[] layerNodeCounts = {10, 10, 1};
-        NeuralNet net = new NeuralNet(layerNodeCounts);
+    public void testPokerHand() throws Exception {//String testLabel, double[][] inputValues, double[][] expectedValues, int iterationLimit, double accuracyLimit) {
 
         //Get file from resources folder
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("neural_net/poker_hand_test_data.txt").getFile());
+        File file = new File(classLoader.getResource("neural_net/poker_hand_training_data.txt").getFile());
 
         try (Scanner scanner = new Scanner(file)) {
-            int limit = 20;
+            int trainLimit = 20000;
             int counter = 0;
-            
-            while (scanner.hasNextLine() && (counter < limit)) {
+            ArrayList<double[]> inputData = new ArrayList<>();
+            ArrayList<double[]> expectedData = new ArrayList<>();
+
+            while (scanner.hasNextLine() && (counter < trainLimit)) {
                 String line = scanner.nextLine();
-                System.out.println(line);
-                
+//                System.out.println(line);
+
                 String[] list = line.split(",");
 
-                int[] inputs = new int[10];
-                for (int i = 0; i < inputs.length; i++) {
-                    inputs[i] = Integer.parseInt(list[i]);
+                double[] input = new double[10];
+                for (int i = 0; i < input.length; i++) {
+                    input[i] = Integer.parseInt(list[i]);
                 }
-                
-                int[] expected = new int[1];
-//                int value = Integer.parseInt(list[10]);
+                inputData.add(input);
+
+                double[] expected = new double[1];
                 expected[0] = Integer.parseInt(list[10]);
-                
+                expectedData.add(expected);
+
                 counter++;
             }
+
             scanner.close();
 
+            double[][] inputs = new double[inputData.size()][10];
+            double[][] expected = new double[expectedData.size()][1];
+            for (int i = 0; i < inputData.size(); i++) {
+                inputs[i] = inputData.get(i);
+                expected[i] = expectedData.get(i);
+            }
+
+            int[] layerNodeCounts = {10, 10, 1};
+            NeuralNet net = new NeuralNet(layerNodeCounts);
+            net.train(inputs, expected, 3);
+
+            
+        //test the model
+        ArrayList<double[]> testData = getDataFromFile("neural_net/poker_hand_test_data.txt", 11, 2000);
+        
+        System.out.println("  Poker Hands");
+        System.out.println("  Expected    Result      Variance");
+
+
+        double[] testInputs = new double[10];
+        double testExpected, variance, testOutput;
+        
+        for (double[] testAll : testData){
+            System.arraycopy(testAll, 0, testInputs, 0, testInputs.length);
+            testExpected = testAll[10];
+            
+            double[] testResult = net.computeOutputs(testInputs);
+            testOutput = testResult[0];
+            variance = testExpected - testOutput;
+            
+            System.out.println(String.format("  %.5f     %.5f     %.5f", testExpected, testOutput, variance));
+        }
+            
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        
+        
+        
 //	return result.toString();
 //        System.out.println("train - " + testLabel);
 //
